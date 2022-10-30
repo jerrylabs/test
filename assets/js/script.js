6d90663ec97a794fc9ecb9184bff28cce3380564
Jaroslav Kameň <jaroslav.kamen@firma.seznam.cz>
2022-10-30 12:29:14 +0100

    Test plevel naslepo

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// var $grid = $('.grid').imagesLoaded( function() {
+//   $grid.masonry({
+//     itemSelector: '.grid-item',
+//     percentPosition: true,
+//     columnWidth: '.grid-sizer',
+//     gutter: 10
+//   });
+// });
+
+$.fancybox.defaults.buttons = ['slideShow', 'fullScreen', 'thumbs', 'close'];
